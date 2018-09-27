720badbe621925450a76217f1d4a17e8f41da898
Zhang yang <ssea.zh@gmail.com>
2018-09-27 16:59:27 +0800

    update item

--- hi-pda/diySpider/spiders/pda.py
+++ hi-pda/diySpider/spiders/pda.py
@@ -52,28 +52,28 @@ class QuotesSpider(scrapy.Spider):
             tid = tempResult.attrib.get('id')
             if tid and tid.startswith("normalthread_"):
                 threadItem = ThreadItem()
-                threadItem.tid = tid.replace("normalthread_", "")
+                threadItem['tid'] = tid.replace("normalthread_", "")
                 # print(tid)
-                threadItem.title = tempResult.xpath('.//span[@id]/a/text()').extract_first()
-                threadItem.turl = tempResult.xpath('.//span[@id]/a/@href').extract_first()    
+                threadItem['title'] = tempResult.xpath('.//span[@id]/a/text()').extract_first()
+                threadItem['turl'] = tempResult.xpath('.//span[@id]/a/@href').extract_first()    
                 # print(title)
                 # print(turl)
 
-                threadItem.with_img_attach = len(tempResult.xpath('.//img[@alt="图片附件"]')) > 0
+                threadItem['with_img_attach'] = len(tempResult.xpath('.//img[@alt="图片附件"]')) > 0
                 # print("hasImageAttach %s"%hasImageAttach)
-                threadItem.with_file_attach = len(tempResult.xpath('.//img[@alt="附件"]')) > 0
+                threadItem['with_file_attach'] = len(tempResult.xpath('.//img[@alt="附件"]')) > 0
                 # print("hasFileAttach %s"%hasFileAttach)
 
-                threadItem.tag = tempResult.xpath('.//em[contains(text(), "[")]/a/text()').extract_first()    
+                threadItem['tag'] = tempResult.xpath('.//em[contains(text(), "[")]/a/text()').extract_first()    
                 # print("tag %s"%tag)
 
-                threadItem.author_uid = tempResult.xpath('.//td[@class="author"]//a/@href').re_first("uid=([0-9]+)")
+                threadItem['author_uid'] = tempResult.xpath('.//td[@class="author"]//a/@href').re_first("uid=([0-9]+)")
                 # print("authorUid %s"%authorUid)
 
-                threadItem.author_username = tempResult.xpath('.//td[@class="author"]//a/text()').extract_first()    
+                threadItem['author_username'] = tempResult.xpath('.//td[@class="author"]//a/text()').extract_first()    
                 # print("author %s"%authorName)
                 
-                threadItem.publish_date = tempResult.xpath('.//td[@class="author"]/em/text()').extract_first()    
+                threadItem['publish_date'] = tempResult.xpath('.//td[@class="author"]/em/text()').extract_first()    
                 # print("%s"%publishDate)
 
                 postCount = tempResult.xpath('.//td[@class="nums"]/strong/text()').extract_first()    
@@ -81,7 +81,7 @@ class QuotesSpider(scrapy.Spider):
                     postCount = int(postCount)
                 except ValueError as e:
                     postCount = -1
-                threadItem.follow_post_count = postCount
+                threadItem['follow_post_count'] = postCount
                 # print("postCount = %s"%int(postCount))
 
                 readCount = tempResult.xpath('.//td[@class="nums"]/em/text()').extract_first()    
@@ -89,13 +89,13 @@ class QuotesSpider(scrapy.Spider):
                     readCount = int(readCount)
                 except ValueError as e:
                     readCount = -1
-                threadItem.read_count = readCount
+                threadItem['read_count'] = readCount
                 # print("readCount = %s"%readCount)
 
-                threadItem.last_post_username = tempResult.xpath('.//td[@class="lastpost"]/cite/a/text()').extract_first()    
+                threadItem['last_post_username'] = tempResult.xpath('.//td[@class="lastpost"]/cite/a/text()').extract_first()    
                 # print("lastPostUser = %s"%lastPostUser)
 
-                threadItem.last_post_date = tempResult.xpath('.//td[@class="lastpost"]/em/a/text()').extract_first()    
+                threadItem['last_post_date'] = tempResult.xpath('.//td[@class="lastpost"]/em/a/text()').extract_first()    
                 # print("lastPostDate = %s"%lastPostDate)
                 print(threadItem)
                 print("================")
